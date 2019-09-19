752aa2e03a60428bbceaa1a50306b247bab7814f
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-19 22:01:55 +0000

    abc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: return error 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
