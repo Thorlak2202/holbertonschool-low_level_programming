36bb3799fbc2c8291b02066e51b26ca2d2ab7ae8
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-19 22:10:45 +0000

    aZ

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: return error 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
