c1672611ff3b87ac4963aff2abf75ab80d1331d7
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-19 20:34:06 +0000

    always check uppercases

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 int n, Last;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("last digit of %d is", n);
+printf("Last digit of %d is", n);
 Last = n % 10;
 if (Last > 5 && Last != 0)
 {
