ac7079833bcd3dbe69a32983fc8cafe2255e74fe
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-19 22:48:09 +0000

    eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: return error 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a != 'e' && a != 'q')
+{
+putchar(a);
+}
+}
+putchar('\n');
+return (0);
+}
