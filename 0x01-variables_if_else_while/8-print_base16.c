c3a552bc544dd2e22d8d629c1254567193d013a7
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-20 00:48:59 +0000

    0tof

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: return error 0
+*/
+int main(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
