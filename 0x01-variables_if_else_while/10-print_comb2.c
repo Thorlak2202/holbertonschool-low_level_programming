dd87ac7256eeb415379632a427b67aeb1353ddd0
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-20 04:25:30 +0000

    comb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
@@ -8,22 +7,18 @@
 */
 int main(void)
 {
-int a, b;
-for (a = '0'; a <= '9'; a++)
-for (b = '0'; b <= '9'; b++)
+int a;
+for (a = 0; a < 100 ; a++)
 {
-putchar (a);
-putchar (b);
-if (b < '9')
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+if (a < 99)
 {
 putchar(',');
 putchar(' ');
 }
 else
-{
 putchar('\n');
-}
 }
 return (0);
 }
-
