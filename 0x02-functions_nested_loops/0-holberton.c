bca97a9703f2f9a436954b5e9c9bd2a965456072
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-23 22:12:01 +0000

    betty style

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 * _putchar - writes the character c to stdout
-* @c: The character to print
+* main - inits the operation
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
@@ -10,8 +10,9 @@ int main(void)
 {
 	int i;
 	char a[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	for(i = 0; i < 9; ++i)
-	_putchar(a[i]);
+
+	for (i = 0; i < 9; ++i)
+		_putchar(a[i]);
 	_putchar('\n');
 	return (0);
 }
