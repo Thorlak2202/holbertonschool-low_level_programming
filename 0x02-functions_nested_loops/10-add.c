c3d6b858e3a548786a58047d3792e87eed38ea0c
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-24 23:03:38 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* add - gets the result of adding ints.
+*
+*@a: first int to be checked.
+*
+*@b: second int to be checked.
+*
+* Return: always 0 whe success.
+*/
+int add(int a, int b)
+{
+int s = a + b;
+return (s);
+}
