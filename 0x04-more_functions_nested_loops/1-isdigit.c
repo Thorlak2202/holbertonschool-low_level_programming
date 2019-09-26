a6663006755856fb30e00403a78143b37f86dea8
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-26 15:02:38 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * _isdigit - checks if the entry char is digit or not
+  *
+  *@c: entry character
+  *
+  * Return: 1 if digit, 0 otherwise.
+  */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
