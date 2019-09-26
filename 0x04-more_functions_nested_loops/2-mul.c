c3322954a32c7363ddd62875de3d02b4b6181eab
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-26 15:17:33 +0000

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * mul - entry integers a and b.
+  *
+  * @a: first entry character
+  * @b: second entry character
+  *
+  * Return: returns the result of mul
+  */
+int mul(int a, int b)
+{
+	int z;
+z = a * b;
+
+return (z);
+}
+
