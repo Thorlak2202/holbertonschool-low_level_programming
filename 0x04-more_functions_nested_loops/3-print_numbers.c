6cf1e5508b4d37a23ea9602c2a0574364798ffc0
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-26 15:31:04 +0000

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * print_numbers - entry integers.
+  *
+  *
+  * Return: 0 if success.
+  */
+void print_numbers(void)
+{
+int x;
+	for (x = 0 ; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+		_putchar('\n');
+}
+
