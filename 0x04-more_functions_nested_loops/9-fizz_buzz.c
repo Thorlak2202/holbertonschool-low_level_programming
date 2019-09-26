e9ccf8fcfc75a673f0c1ded9e119f48a39b2e459
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-26 20:45:27 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  * main - init the operation.
+  *
+  *
+  * Return: 0 if success.
+  */
+int main(void)
+{
+int x;
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else
+		{
+		printf("%d", x);
+		printf(" ");
+		}
+	}
+printf("\n");
+return (0);
+}
