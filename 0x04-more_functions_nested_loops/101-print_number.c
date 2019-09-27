9becb00f0606da2eded0c51b49446e8444adbd42
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-27 00:29:50 +0000

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+  * print_number - entry values to print.
+  *
+  * @n: values to print.
+  *
+  * Return: returns the result of mul
+  */
+void print_number(int n)
+{
+int y;
+
+	if (n > 999)
+{
+	_putchar((n / 1000) + '0');
+	_putchar(((n / 100) % 10) + '0');
+	_putchar(((n / 10) % 10) + '0');
+	_putchar((n % 10) + '0');
+}
+else if (n < 999 && n > 99)
+{
+	_putchar((n / 100) + '0');
+	_putchar((n / 10 % 10) + '0');
+	_putchar((n % 10) + '0');
+}
+else if (n < 99 && n > 9)
+{
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
+}
+else if (n < 9 && n >= 0)
+{
+	_putchar((n % 10) + '0');
+}
+else if (n < 0)
+{
+	y = n * (-1);
+	_putchar(45);
+	_putchar((y / 10) + '0');
+	_putchar((y % 10) + '0');
+}
+}
