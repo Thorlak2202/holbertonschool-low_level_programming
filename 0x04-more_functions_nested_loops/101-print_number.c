8856a78558657f08003fa03141e47476767b7833
Thorlak2202 <darkdrakkar@gmail.com>
2019-09-27 01:58:12 +0000

    works?

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -20,7 +20,7 @@ int y;
 else if (n <= 999 && n > 99)
 {
 	_putchar((n / 100) + '0');
-	_putchar(((n / 10 ) % 10) + '0');
+	_putchar(((n / 10) % 10) + '0');
 	_putchar((n % 10) + '0');
 }
 else if (n <= 99 && n > 9)
