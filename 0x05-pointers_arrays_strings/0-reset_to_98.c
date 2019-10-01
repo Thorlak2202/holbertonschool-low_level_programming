46189ffd5c60e59891e36eeb2c492c34e6df0e5d
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-01 03:34:11 +0000

    preparing repo for the tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* reset_to_98 - updates the value of an int to 98.
+*
+* @*n: entry pointer character
+*
+* Return: 0 always when success.
+*/
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#this will inclue txt
