1367c2d4d00dc0cda31f23c572d65708293423c4
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-02 19:23:49 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _strlen - prints the lenght of a string.
+*
+* @s: entry pointer character
+*
+* Return: returns the lenght of the string.
+*/
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != 0; i++)
+{
+}
+return (i);
+}
