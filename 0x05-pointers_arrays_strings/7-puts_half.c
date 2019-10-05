1d999b3a2daded32b2a3a5df93f6913d535da161
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-05 21:18:38 +0000

    fixed solution

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,14 @@ int i, l, count;
 
 	for (l = (count / 2); str[l] != '\0'; l++)
 	{
+		if ((count % 2) != 0)
+		{
+			_putchar(str[l + 1]);
+		}
+		else
+		{
 		_putchar(str[l]);
+		}
 	}
 _putchar('\n');
 }
