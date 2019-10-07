45c0e32ad713402bcab427f6fdec76b56a9cb876
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-07 19:26:00 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _memcpy - copies n bytes from memory area src to memory area dest.
+*
+* @src: entry values.
+* @dest: memory area destiny.
+* @n: n bytes to copy.
+*
+* Return: returns the pointer value.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+	for (i = 0; i < n && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+return (dest);
+}
