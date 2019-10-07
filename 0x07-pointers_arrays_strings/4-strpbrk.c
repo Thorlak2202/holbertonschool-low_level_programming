d5c4232d4c359de5e2c5febd7ed3a2bab36e1f22
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-07 22:25:24 +0000

    llo, world

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* _strpbrk - Returns a pointer to occurrence of c in the string s.
+*
+* @s: entry values.
+* @accept: character to evaluate with.
+*
+* Return: returns the pointer value.
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+	for (i = 0; s[i] != '0'; i++)
+	{
+		for (j = 0; accept[j] != 0; j++)
+		{
+		if (s[i] == accept[j])
+		{
+		s += i;
+		return (s);
+		}
+		}
+	}
+return ('\0');
+}
