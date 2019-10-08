771ed04ccfa96ed873eedfe60a6e628c97d35533
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-08 01:39:56 +0000

    finally?

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,16 +13,28 @@ char *_strstr(char *haystack, char *needle)
 {
 int i, j;
 
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+		if (haystack[i + j] != needle[j])
 		{
-		if (haystack[i] == needle[0])
-		{
-		haystack += i;
-		return (haystack);
+			break;
 		}
+		else if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+		else if (haystack[i] == needle[0] && haystack[i + 1] == needle[1])
+		{
+			haystack += i;
+			return (haystack);
 		}
 	}
-return ('\0');
+	}
+return (0);
 }
