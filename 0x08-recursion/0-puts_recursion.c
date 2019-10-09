968c67e3d74fac35152429190085fa678458968c
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-09 16:15:10 +0000

    all set

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts-recursion - prints a string, followed by a new line.
+ *
+ * @s: entry value string.
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == 0)
+{
+	_putchar('\n');
+}
+else
+{
+ _putchar(*s);
+ _puts_recursion(s + 1);
+}
+}
