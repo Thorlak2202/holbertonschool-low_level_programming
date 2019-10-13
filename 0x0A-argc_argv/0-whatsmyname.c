340463f4f5613d802499e5e55345d8e4f8a3108c
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-13 15:52:26 +0000

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* _isupper - checks if the entry char is uppercase or not
+*
+*@c: entry character
+*
+* Return: 1 if uppercase, 0 otherwise.
+*/
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
