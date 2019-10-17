e915b60ea7b90fb22e941e85a01b3068d4e853b2
Chris <DarkDrakkar@gmail.com>
2019-10-17 15:38:55 -0500

    array arrange

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *array_range - creates an array with the character sent by parameter.
+*
+* @min: min element sent by parameter.
+* @max: max element sent by parameter.
+*
+* Return: return NULL when function fails.
+*/
+int *array_range(int min, int max)
+{
+	int *p = 0, i = 0;
+
+if (min > max)
+{
+	return (NULL);
+}
+p = malloc(sizeof(int) * (max - min));
+if (p == NULL)
+{
+	return (NULL);
+}
+for (i = min; i <= max; i++)
+{
+	p[i] = i;
+}
+return (p);
+}
