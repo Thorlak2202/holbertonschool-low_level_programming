30942011c24c8edd50d8ed1878bfd90dd4f81a27
Chris <DarkDrakkar@gmail.com>
2019-10-18 17:38:05 -0500

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,9 +13,11 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int *p;
+
 if (new_size > old_size)
 {
-	ptr = malloc(sizeof(old_size) + (new_size - old_size));
+	p = malloc(sizeof(old_size) + (new_size - old_size));
 	if (ptr == NULL)
 	{
 		return (NULL);
@@ -27,8 +29,8 @@ else if (new_size == old_size)
 }
 else if (ptr == NULL)
 {
-	ptr = malloc(new_size);
-	if (ptr == NULL)
+	p = malloc(new_size);
+	if (p == NULL)
 	{
 		return (NULL);
 	}
@@ -38,5 +40,6 @@ else if (new_size == 0 && ptr != NULL)
 free(ptr);
 return (NULL);
 }
-return (ptr);
+free(ptr);
+return (p);
 }
