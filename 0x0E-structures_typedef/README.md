31f1b470981e46cbb149ede46976ac3a8cc129fd
Chris <DarkDrakkar@gmail.com>
2019-10-21 15:54:49 -0500

    who is a good boy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+# this is going to have text
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/**
+ * struct dog - data of a cute doggie.
+ *
+ * @name: the name of the pup!.
+ * @age: the age in human of the doggie.
+ * @owner: the owner of the pup.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
