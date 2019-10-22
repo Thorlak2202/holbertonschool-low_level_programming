b587c76125dbc24207cd6d1645f5924ef1a35d7f
Chris <DarkDrakkar@gmail.com>
2019-10-21 19:29:07 -0500

    prints holby

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define __HOLBY__ "Hello, Holberton"
+/**
+ * main - prints a string
+ *
+ * Return: 0 when success.
+ */
+int main(void)
+{
+printf("%s\n", __HOLBY__);
+return (0);
+}
