477e28592c6a1638b922b3d9cb63b49cf2312e18
Chris <DarkDrakkar@gmail.com>
2019-10-23 13:58:23 -0500

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - prints the index of an int on an array when matches.
+ *
+ * @size: size of the array.
+ * @array: array to be evaluated.
+ * @cmp: action to be performed
+ *
+ * Return: 0 when success.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array != NULL && size != 0 && cmp != NULL)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]))
+			return (i);
+			i++;
+		}
+	}
+	return (i);
+}
