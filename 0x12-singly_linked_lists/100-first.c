1500ac9365dd6344469a39874f6ebee12e8607e8
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-31 17:45:55 -0500

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* print_first - prints before main.
+*
+*/
+void print_first(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore
+my house upon my back!\n");
+}
+
