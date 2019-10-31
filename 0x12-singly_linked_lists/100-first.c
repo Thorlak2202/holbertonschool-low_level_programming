01e07e5be40c964bd560a19147c2a00313421768
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-31 17:55:37 -0500

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,19 @@
 */
 void print_first(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore
-my house upon my back!\n");
-}
+char s[] = "You're beat! and yet, you must allow,";
+char s2[] = "I bore my house upon my back!";
+int i, j;
 
+for (i = 0; s[i] != '\0'; i++)
+{
+putchar(s[i]);
+}
+putchar('\n');
+
+for (i = 0; s2[j] != '\0'; j++)
+{
+putchar(s2[j]);
+}
+putchar('\n');
+}
