e7adb504973ceac5acfd763a1f5ee45a7c7bf80a
Chris <DarkDrakkar@gmail.com>
2019-10-31 12:34:20 -0500

    added an if contition to evaluate the null case in h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,22 +12,23 @@
 size_t print_list(const list_t *h)
 {
 unsigned long count = 0;
-
-while (h != NULL)
+if (h != NULL)
 {
-if (h->str != NULL)
-{
-printf("[%d]%s\n", h->len, h->str);
-h = h->next;
-count++;
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+		printf("[%d]%s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	else
+	{
+		printf("[%d]%s\n", 0, "(nil)");
+		h = h->next;
+		count++;
+	}
+	}
 }
-else
-{
-printf("[%d]%s\n", 0, "(nil)");
-h = h->next;
-count++;
-}
-}
-
 return (count);
 }
