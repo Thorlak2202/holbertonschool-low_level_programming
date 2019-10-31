8789b8be3441538a1efb2d6ad461f5b1d340eaa6
Thorlak2202 <darkdrakkar@gmail.com>
2019-10-31 15:44:20 -0500

    changed the strleng for a counter in file 2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
+int i = 0;
 
 new = malloc(sizeof(list_t));
 if (new == NULL)
@@ -23,7 +24,11 @@ if (new == NULL)
 new->str = strdup(str);
 new->next = *head;
 *head = new;
-new->len = strlen(str);
+while (str[i])
+{
+i++;
+}
+new->len = i;
 
 return (new);
 }
