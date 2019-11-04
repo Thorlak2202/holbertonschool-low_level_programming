04be78170379ce0329f25639bfd6e4ff1e0c1a99
Chris <DarkDrakkar@gmail.com>
2019-11-04 11:49:28 -0500

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* free_listint2 - frees malloc of the functions.
+*
+* @head: pointer to the start of the list.
+*
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	free(*head);
+}
