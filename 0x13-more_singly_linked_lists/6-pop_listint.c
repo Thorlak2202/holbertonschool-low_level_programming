b9c58211bd3b9c73c84a5ab0944e9afa0a2caec4
Chris <DarkDrakkar@gmail.com>
2019-11-04 13:17:13 -0500

    delete node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* pop_listint - frees malloc of the functions.
+*
+* @head: pointer to the start of the list.
+*
+* Return: returns the head node’s data.
+*/
+int pop_listint(listint_t **head)
+{
+listint_t *new;
+int i = 0;
+
+
+if (head == NULL)
+{
+	return (0);
+}
+new = *head;
+*head = (*head)->next;
+i = new->n;
+free(new);
+
+return (i);
+}
