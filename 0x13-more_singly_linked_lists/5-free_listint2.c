8f443c67c37b3485a7197ad7cdc04d3ce0a9218e
Chris <DarkDrakkar@gmail.com>
2019-11-05 10:55:35 -0500

    betty being ridiculous about unexistent errors fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,19 +12,21 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-if (head != NULL)
-{
-while (*head != NULL)
-{
-	temp = *head;
-	*head = temp->next;
-	free(temp);
-}
-}
-else
-{
-	return;
-}
-free(*head);
-(*head) = NULL;
+
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
+	}
+
+	free(*head);
+	(*head) = NULL;
 }
