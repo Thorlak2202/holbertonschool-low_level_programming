38bab56affedbc76abfa128e470d990ae5f1b137
Chris <DarkDrakkar@gmail.com>
2019-11-05 13:22:17 -0500

    return fixed in task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	}
 
-	if (tmp != NULL && i == index)
+	if (tmp != NULL && i == index && index == 0)
 	{
 		*head = tmp->next;
 		free(tmp);
@@ -34,10 +34,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		prev = tmp;
 		tmp = tmp->next;
 		i++;
+		return (1);
 	}
 	prev->next = tmp->next;
 	tmp->next = prev;
 	free(tmp);
 
-return (1);
+return (-1);
 }
