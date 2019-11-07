85aa153e57a9abd06e7b4d2eabdc09c94425fe85
Chris <DarkDrakkar@gmail.com>
2019-11-07 18:18:38 -0500

    moved

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,11 +21,11 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = i - 1; i >= 0; i--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b == NULL)
 		{
 			return (0);
 		}
-		if (b == NULL)
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
