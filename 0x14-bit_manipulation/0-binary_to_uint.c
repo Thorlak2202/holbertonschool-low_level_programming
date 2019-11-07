6f00a69fe8ad06777210352f0c8086c948a8264d
Chris <DarkDrakkar@gmail.com>
2019-11-07 18:08:53 -0500

    task 0 edge case added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = i - 1; i >= 0; i--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if ((b[i] != '0' && b[i] != '1') || b == NULL)
 		{
 			return (0);
 		}
