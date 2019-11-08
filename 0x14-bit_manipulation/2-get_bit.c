48fd5f4c3c5b6afc0e5f492f4f3f4955c7f3e236
Thorlak2202 <darkdrakkar@gmail.com>
2019-11-07 22:02:41 -0500

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* get_bit - returns the value of a bit at a given index.
+*
+* @index: index, starting from 0 of the bit.
+* @n: data.
+*
+* Return: the value of the bit at index index or -1 if an error occured.
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int count = 0, value;
+	unsigned long int i;
+
+if (n != '\0')
+{
+	while ((i) > 1)
+	{
+		i = i / 10;
+		count++;
+	}
+	value = (n >> index) & 1;
+
+return (value);
+}
+else
+	return (-1);
+}
