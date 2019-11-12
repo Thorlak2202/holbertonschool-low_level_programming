3b9b1967af3a9a0fb12f3d0079b41b90b3f51a65
Chris <DarkDrakkar@gmail.com>
2019-11-12 14:22:08 -0500

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0;
 
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	if (fd == -1)
 	{
