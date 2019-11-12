477c0c8764ebd4a4cc097616564dd2d8da9e4379
Chris <DarkDrakkar@gmail.com>
2019-11-12 11:59:39 -0500

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "holberton.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+*
+* @filename: pointer to the name of the file.
+* @letters: is the number of letters it should read and print.
+*
+* Return: 1 if it worked, or 0 if an error occurred.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t count = 0;
+	char buf[1024];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	count += read(fd, buf, letters);
+	buf[count] = '\0';
+
+	dprintf(STDIN_FILENO, "%s", buf);
+
+	close(fd);
+
+	return (count);
+}
