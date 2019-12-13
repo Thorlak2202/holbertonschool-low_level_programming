574103ca1c059231bf57e1fc117ada0b6124a737
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2019-12-13 10:42:47 -0500

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* free_dlistint - frees malloc of the functions.
+*
+* @head: pointer to the start of the list.
+*
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
