6f1db103d2f8609246972bafb2389a5cc90a3b67
Thorlak2202 <darkdrakkar@gmail.com>
2020-01-18 15:25:35 -0500

    newline

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 /**
- * struct hash_node_s - Node of a hash table
+ * struct hash_node_s - Node  of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
