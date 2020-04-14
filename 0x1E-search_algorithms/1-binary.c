52cea5bddf128d9ea55a4833009222b9a0bd031e
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-14 12:49:14 -0500

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - function that uses the binary search algorithm.
+ *
+ * @array: array to search into.
+ * @size: size value of the array.
+ * @value: value to be searched.
+ *
+ * Return: value on succes, -1 on failure.
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+int mid, i;
+int first = 0;
+int last = size - 1;
+mid = (first + last) / 2;
+
+while (first <= last)
+{
+	printf("Searching in array:");
+	for (i = first; i < last; i++)
+		printf("%i, ", array[i]);
+	printf("%i\n", array[i]);
+	if (array[mid] < value)
+	{
+		first = mid + 1;
+	}
+	else if (array[mid] == value)
+	{
+		return (mid);
+	}
+	else
+	{
+		last = mid - 1;
+	}
+
+	mid = (first + last) / 2;
+}
+if (first > last)
+	return (-1);
+return (0);
+}
