6461c80db219ce2901e951088cfbb4719f16ca63
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-15 12:30:48 -0500

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,6 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-int _putchar(char c);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
